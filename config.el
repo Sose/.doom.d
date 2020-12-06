b2848c1123ed540d4e317f8b2887e5fd35e3f498
Joni Hiltunen <djonih@gmail.com>
2020-12-06 23:59:17 +0200

    Mail

--- config.el
+++ config.el
@@ -7,7 +7,7 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Joni Hiltunen"
-      user-mail-address "Joni Hiltunen")
+      user-mail-address "djonih@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
