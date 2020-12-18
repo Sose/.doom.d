cc135ec866d604cb9117c7d34381038e2a490c2b
Joni Hiltunen <djonih@gmail.com>
2020-12-18 08:54:39 +0200

    SPC c X toggles lsp-flycheck-ui

--- config.el
+++ config.el
@@ -20,7 +20,7 @@
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 (setq doom-font (font-spec :family "Source Code Pro" :size 12 :weight 'regular)
-       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+      doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -105,10 +105,17 @@
          :desc "Treemacs"
          "t" #'treemacs)))
 
+(defun sose/lsp-ui-toggle ()
+  "Toggle LSP UI"
+  (interactive)
+  (if (get-buffer "*lsp-diagnostics*")
+      (lsp-ui-flycheck-list--quit)
+    (lsp-ui-flycheck-list)))
+
 (map! :leader
       (:prefix "c"
        :desc "Show all errors"
-       "X" #'lsp-ui-flycheck-list))
+       "X" #'sose/lsp-ui-toggle))
 
 ;; My custom stuff
 (setq
